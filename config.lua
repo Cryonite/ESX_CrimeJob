e659a388368dc4c3fa90d224c368d627fb77c1db
Cryonite <zeppzakelijk@gmail.com>
2021-07-12 12:46:12 +0200

    files

---
+++ config.lua
@@ -0,0 +1,74 @@
+Config                            = {}
+Config.DrawDistance               = 100.0
+Config.MarkerType                 = 1
+Config.MarkerSize                 = { x = 1.5, y = 1.5, z = 1.0 }
+Config.MarkerColor                = { r = 50, g = 50, b = 204 }
+Config.EnablePlayerManagement     = true
+Config.EnableArmoryManagement     = true
+Config.EnableESXIdentity          = true -- only turn this on if you are using esx_identity
+Config.EnableNonFreemodePeds      = false -- turn this on if you want custom peds
+Config.EnableSocietyOwnedVehicles = false
+Config.EnableLicenses             = false
+Config.MaxInService               = -1
+Config.Locale                     = 'en'
+
+Config.MafiaStations = {
+
+  Mafia = {
+
+    Blip = {
+      Pos     = { x = 425.130, y = -979.558, z = 30.711 },
+      Sprite  = 60,
+      Display = 4,
+      Scale   = 1.2,
+      Colour  = 29,
+    },
+
+    AuthorizedWeapons = {
+      { name = 'WEAPON_COMBATPISTOL',     price = 4000 },
+      { name = 'WEAPON_ASSAULTSMG',       price = 15000 },
+      { name = 'WEAPON_ASSAULTRIFLE',     price = 25000 },
+      { name = 'WEAPON_SWITCHBLADE',      price = 500 },
+	  
+    },
+
+	  AuthorizedVehicles = {
+		  { name = 'btype',      label = 'Roosevelt' },
+	  },
+
+    Cloakrooms = {
+      { x = 9.283, y = 528.914, z = 169.635 },
+    },
+
+    Armories = {
+      { x = 1.550, y = 527.397, z = 169.617 },
+    },
+
+    Vehicles = {
+      {
+        Spawner    = { x = 14.40, y = 550.1, z = 176.187 },
+        SpawnPoint = { x = 8.237, y = 556.963, z = 175.266 },
+        Heading    = 90.0,
+      }
+    },
+	
+	  Helicopters = {
+      {
+        Spawner    = { x = 20.312, y = 535.667, z = 173.627 },
+        SpawnPoint = { x = 3.40, y = 525.56, z = 177.919 },
+        Heading    = 0.0,
+      }
+    },
+
+    VehicleDeleters = {
+      { x = 22.74, y = 545.9, z = 175.027 },
+      { x = 21.35, y = 543.3, z = 175.027 },
+    },
+
+    BossActions = {
+      { x = 4.113, y = 526.897, z = 173.628 }
+    },
+
+  },
+}
+
